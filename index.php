ce7b2a0f7250a2f2c5f0855494f1fe645821a43a
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-11-22 12:37:21 +0100

    Sobstitute parts with templates to avoid code repeat

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="it">
+    <head>
+        <title>My Unibo Vinyl</title>
+        <?php require("templates/head-section.php"); ?>
+    </head>
+    <body>
+        <?php require("templates/page-header.php"); ?>
+        <main>
+            <section>
+                <h1>Vinili</h1>
+            </section>
+            <section>
+                <h1>CD</h1>
+            </section>
+            <section>
+                <h1>Giradischi</h1>
+            </section>
+            <section>
+                <h1>Bundle</h1>
+            </section>
+        </main>
+    </body>
+</html>
