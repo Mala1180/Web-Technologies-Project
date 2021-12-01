f263cef037b2618a13092871c0eeff02fea5f1da
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-12-01 18:52:56 +0100

    Add helper methods

--- public/js/RequestHelper.js
+++ public/js/RequestHelper.js
@@ -1,17 +1,53 @@
 /**
 *	Simple class for making ajax requests.
 */
-const BASE_URL = "./";
+const BASE_URL = "./server/";
 
-class RequestHelper {	
-	constructor() {}
-	
+function getDefaultHeaders() {
+	authorizationToken = localStorage.getItem("AuthToken");
+	const headers = {};
+	if (authorizationToken != null) {
+		headers.Authorization = "Bearer " + authorizationToken;
+	}
+	return headers;
+}
+
+function makeUrl(name) {
+	return BASE_URL + name + ".php";
+}
+
+class RequestHelper {
+	constructor() { }
+
 	makeRequest(method, action, myHeaders, parameters = []) {
-		return fetch('./' + action + '.php', {
-		  method: method,
-		  headers: myHeaders,
-		  body: JSON.stringify(parameters)
+		return fetch(BASE_URL + action + '.php', {
+			method: method,
+			headers: myHeaders,
+			body: JSON.stringify(parameters)
 		});
+	}
+	/* We need to add Authorization header for every request */
+	get(url, action, data, callback) {
+		data.action = action;
+		$.ajax({
+			url: makeUrl(url),
+			data: data,
+			success: callback,
+			dataType: "json",
+			headers: getDefaultHeaders()
+		});
+	}
+
+	post(url, action, data, callback) {
+		data.action = action;
+		$.ajax({
+			method: "POST",
+			url: makeUrl(url),
+			data: data,
+			success: callback,
+			dataType: "json",
+			headers: getDefaultHeaders()
+		})
 	}
 }
 const reqHelper = new RequestHelper();
