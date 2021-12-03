1671069da46170e267429267ff42fa50e48d1d72
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-12-03 16:38:29 +0100

    js and jquery refactor

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,18 @@
+$(document).ready(function () {
+    $("#SubmitLogin").click((e) => {
+        e.preventDefault();
+        login(txtUsername.value, txtPassword.value);
+    });
 
-document.querySelector('#SubmitLogin').addEventListener('click', async (e) => {
-  e.preventDefault();
-  login(txtUsername.value, txtPassword.value);
+    function login(username, password) {
+        reqHelper.post("login", "", {
+            "username": username,
+            "password": password
+        }, function (data) {
+            console.log(data);
+        });
+        /*
+         * TODO: Set AuthToken in localStorage
+         */
+    }
 });
-
-async function login(username, password) {
-  const res = await reqHelper.makeRequest("POST", "login",
-    {'Content-type' : 'application/x-www-form-urlencoded; charset=UTF-8'},
-    {"username": username, "password": password});
-
-  if (res.status >= 200 && res.status <= 299) {
-    jwt.setJWT(await res.text());
-  } else {
-    console.log(res.status, res.statusText);
-  }
-}
