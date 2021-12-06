6532c448465d4c9496583075e27a21ddf51d4ff4
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2021-12-06 18:25:26 +0100

    Add user verification methods

--- server/validate.php
+++ server/validate.php
@@ -10,12 +10,25 @@ function validate($JSONWebToken) {
     if(isset($JSONWebToken) && $JSONWebToken) {
         $token = JWT::decode((string)$JSONWebToken, SECRET_KEY, [JWT_CRYPTO_ALGORITHM]);
         $now = new DateTimeImmutable();
-
-        if ($token->iss !== SERVER_NAME || $token->nbf > $now->getTimestamp())
-        {
-            return false;
-        }
-        return true;
+        return $token->iss === SERVER_NAME && $token->nbf < $now->getTimestamp()  
     }
+}
+
+/*
+ * Gets the the jwt token from authorization header 
+ */
+function get_token_from_bearer($authorization) {
+    return substr($authorization, 7);
+}
+
+/*
+ * Checks if an authorization header is presente and if
+ * the token is valid 
+ */
+function is_user_logged() {
+    if (!isset($request_headers["Authorization"])) {
+        return false;
+    }
+    return validate(get_token_from_bearer($request_headers["Authorization"]));
 }
 ?>
