526d81da1dc1c1b80097fcfa9286e3a951c898a0
Alberto Paganelli <albi1600@gmail.com>
2022-01-21 14:36:21 +0100

    corrected connector

--- server/db/dbconnector.php
+++ server/db/dbconnector.php
@@ -6,13 +6,8 @@
 	header('Content-Type: application/json; charset=utf-8');
 	$servername = "192.168.64.2";
 	$database = "UniboVinyl";
-<<<<<<< HEAD
 	$username = "root";
 	$password = "root";
-=======
-	$username = "prova";
-	$password = "prova";
->>>>>>> alberto
 	$port = 3306;
 
 	$db = new mysqli($servername, $username, $password, $database, $port);
