3c6de0df982bc5cd241481ec337bfc5f0d9b06e0
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-01-22 18:26:22 +0100

    restyle product add

--- userVendor.php
+++ userVendor.php
@@ -4,69 +4,77 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-
+    <?php require_once("templates/head-section.php") ?>
     <title>Unibo Vinyl - Venditore</title>
+    <link rel="stylesheet" href="./public/css/userVendor.css">
 </head>
 <body>
-    <header>
-    <!-- wait for mala -->
-    </header>
     <main>  
         <section>
-            <h1>Inserisci nuovo album</h1>
-            <label for="txtTitle">Titolo:</label>
-            <input type="text" id="txtTitle" required/><br>
-            <label for="txtDescription">Descrizione:</label>
-            <input type="text" id="txtDescription" required/><br>
-            <label for="txtGenre">Genere:</label>
-            <select id="selectGenre" required>
-            </select><br>
-            <label for="numSongs">Numero brani:</label>
-            <input type="number" id="numSongs" required/><br>
-            <input type="button" id="confirmSongs" value="Conferma"/>
+            <h1>Nuovo prodotto</h1>
+            <form>
+                <fieldset>
+                    <legend>Dati album</legend>
+                    <ul>
+                        <li>
+                            <label for="txtTitle">Titolo</label>
+                            <input type="text" id="txtTitle" placeholder="Titolo album" required/>
+                        </li>
+                        <li>
+                            <label for="txtDescription">Descrizione</label>
+                            <textarea id="txtDescription" placeholder="descrizione" required></textarea>
+                        </li>
+                        <li>
+                            <label for="txtGenre">Genere</label>
+                            <select id="selectGenre" required>
+                                <option value="">Genere...</option>
+                                <option value="vedo">gatto</option>
+                            </select>
+                        </li>
+                    </ul>
+                </fieldset>
+                <fieldset>
+                    <legend>Brani</legend>
+                    <ul></ul>
+                    <input type="button" id="addSong" value="Aggiungi brano" />
+                </fieldset>
+                <fieldset>
+                    <legend>Opzioni di vendita</legend>
+                    <ul>
+                        <li>
+                            <input type="checkbox" id="checkCD" name="checkCD" value="CD">
+                            <label class="visible" for="checkCD">CD</label>
+                        </li>
+                        <li>
+                            <label for="numCDCopy">Numero copie CD</label>
+                            <input type="number" placeholder="Copie CD" id="numCDCopy">
+                            <label for="priceCD">Prezzo CD</label>
+                            <input type="number" placeholder="Prezzo CD" id="priceCDCopy">
+                            <label for="txtCDProductDescription">Descrizione CD</label>
+                            <textarea id="txtCDProductDescription" placeholder="descrizione"></textarea>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="checkVinyl" name="checkVinyl" value="Vinile">
+                            <label class="visible" for="checkVinyl">Vinile</label>
+                        </li>
+                        <li>
+                            <label for="numVinylCopy">Numero copie Vinili</label>
+                            <input type="number" placeholder="Copie vinile" id="numVinylCopy">
+                            <label for="priceVinyl">Prezzo vinile</label>
+                            <input type="number" placeholder="Prezzo vinile" id="priceVinyl">
+                            <label for="txtVinylProductDescription">Descrizione vinile</label>
+                            <textarea id="txtVinylProductDescription" placeholder="descrizione"></textarea>
+                        </li>
+                        <li>
+                            <input type="button" id="btnConfirm" value="Metti in vendita"/>
+                        </li>
+                    </ul>
+                </fieldset>
+            </form>
         </section>
-
-        <br>
-
-        <section id="sectionSongs">
-        </section>
-
-        <section>
-            <h1>Tipo prodotto</h1>
-            
-            <input type="checkbox" id="checkCD" name="checkCD" value="CD">
-            <label for="checkCD">CD</label>
-            <input type="number" placeholder="Copie CD" id="numCDCopy"><br>
-            <label for="txtCDProductDescription">Descrizione CD</label>
-            <input type="text" id="txtCDProductDescription"/><br>
-            <label for="priceCD">Prezzo CD</label>
-            <input type="number" placeholder="Prezzo CD" id="priceCDCopy"><br><br>
-            
-            <input type="checkbox" id="checkVinyl" name="checkVinyl" value="Vinile">
-            <label for="checkVinyl">Vinile</label>
-            <input type="number" placeholder="Copie vinile" id="numVinylCopy"><br>
-            <label for="txtVinylProductDescription">Descrizione vinile</label>
-            <input type="text" id="txtVinylProductDescription"/><br>
-            <label for="priceVinyl">Prezzo vinile</label>
-            <input type="number" placeholder="Prezzo vinile" id="priceVinylCopy"><br>
-
-            <input type="button" id="btnConfirm" value="Metti in vendita"/><br>
-            
-        </section>
-
-
-        
-
-
     </main>
     <script src="./public/libraries/jquery3.6.0.js"></script>
-    <script type="text/javascript" src="./public/js/RequestHelper.js"></script>
-    <script type="text/javascript" src="./public/js/myjwtclass.js"></script>
-    <script type="text/javascript" src="./public/js/profile.js"></script>
     <script type="text/javascript" src="./public/js/vendor.js"></script>
-    
-    
 </body>
 </html>
 
